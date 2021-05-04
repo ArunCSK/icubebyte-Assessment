a5d4d07fc8546e08cae1e3d7bfd12e37e7ffcfbe
Arun Subburaj <Arun.Subburaj@KALELOGISTICS.IN>
2021-05-04 12:47:33 +0530

    FilePath blank condition added

--- Program.py
+++ Program.py
@@ -3,8 +3,8 @@ from LoadAndFetchCustomer import *
 def FetchCountryData():
     country = input("Enter Country code: ")
     filePath = IsValidCountryName(country)
-    LoadCountryData(filePath)
-    # print(country)
+    if (filePath != ""):
+        LoadCountryData(filePath)
 
 if __name__ == "__main__":
     Load()
